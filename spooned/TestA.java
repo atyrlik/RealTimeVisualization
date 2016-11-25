fd4b5fa8af69daebbf83f6bd20200d7a369c3d63
alexandre <alexadre>
2016-11-25 11:13:05 +0100

    Add type of object created in logs.

--- spooned/TestA.java
+++ spooned/TestA.java
@@ -6,15 +6,15 @@
 
 public class TestA {
     public TestA() {
-        System.out.println("Begin creation");
+        System.out.println("Begin java.lang.String creation");
         new java.lang.String();
-        System.out.println("End creation");
-        System.out.println("Begin creation");
+        System.out.println("End java.lang.String creation");
+        System.out.println("Begin java.lang.String creation");
         java.lang.String o1 = new java.lang.String();
-        System.out.println("End creation");
-        System.out.println("Begin creation");
+        System.out.println("End java.lang.String creation");
+        System.out.println("Begin java.lang.Object creation");
         java.lang.Object o = new java.lang.Object();
-        System.out.println("End creation");
+        System.out.println("End java.lang.Object creation");
     }
 }
 
--- src/main/java/spoon/processor/ObjectCreationProcessor.java
+++ src/main/java/spoon/processor/ObjectCreationProcessor.java
@@ -5,6 +5,7 @@ import spoon.reflect.code.CtBlock;
 import spoon.reflect.code.CtCodeSnippetStatement;
 import spoon.reflect.code.CtConstructorCall;
 import spoon.reflect.code.CtStatement;
+import spoon.reflect.reference.CtTypeReference;
 
 /**
  * Created by alexandre on 18/11/16.
@@ -19,22 +20,17 @@ public class ObjectCreationProcessor extends AbstractProcessor<CtConstructorCall
 
         // if affectation ("O o = new O()")
         if(!(element.getParent(CtStatement.class) instanceof CtBlock)){
-//            element.getParent(CtStatement.class).insertBefore(toPrintBefore);
-//            element.getParent(CtStatement.class).insertAfter(toPrintAfter);
-            addLogs(element.getParent(CtStatement.class));
+            addLogs(element.getParent(CtStatement.class), element.getType());
         }
         // if new alone
         else {
-//            element.insertBefore(toPrintBefore);
-//            element.insertAfter(toPrintAfter);
-//            System.out.println(element.getType());
+            addLogs(element, element.getType());
         }
     }
 
-    private void addLogs(CtStatement element){
-
-        CtCodeSnippetStatement toPrintBefore = this.getFactory().Code().createCodeSnippetStatement("System.out.println(\"Begin creation\")");
-        CtCodeSnippetStatement toPrintAfter = this.getFactory().Code().createCodeSnippetStatement("System.out.println(\"End creation\")");
+    private void addLogs(CtStatement element, CtTypeReference type){
+        CtCodeSnippetStatement toPrintBefore = this.getFactory().Code().createCodeSnippetStatement("System.out.println(\"Begin " + type + " creation\")");
+        CtCodeSnippetStatement toPrintAfter = this.getFactory().Code().createCodeSnippetStatement("System.out.println(\"End " + type +" creation\")");
 
         element.insertBefore(toPrintBefore);
         element.insertAfter(toPrintAfter);
