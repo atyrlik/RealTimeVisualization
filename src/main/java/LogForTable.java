43bc56fccbe318e1aac687eb65fd61165c1997f4
dduvacher <dduvacher@gmail.com>
2016-12-01 09:34:50 +0100

    Add new properties and add new clolumn

--- src/main/java/LogForTable.java
+++ src/main/java/LogForTable.java
@@ -4,18 +4,38 @@ import javafx.beans.property.SimpleStringProperty;
  * Created by dduvacher on 01/12/16.
  */
 public class LogForTable {
-    private SimpleStringProperty label;
+    private SimpleStringProperty objectName;
+    private SimpleStringProperty className;
+    private SimpleStringProperty currentTime;
 
-    public LogForTable(String label)
+    public LogForTable(String oName,String cName,String time)
     {
-        this.label= new SimpleStringProperty(label);
+        this.objectName= new SimpleStringProperty(oName);
+        this.className = new SimpleStringProperty(cName);
+        this.className = new SimpleStringProperty(time);
     }
 
-    public String getLabel(){
-        return label.get();
+    public String getObjectName(){
+        return objectName.get();
     }
 
-    public void setLabel(String label){
-        this.label = new SimpleStringProperty(label);
+    public void setObjectName(String oName){
+        objectName= new SimpleStringProperty(oName);
+    }
+
+    public String getClassName(){
+        return className.get();
+    }
+
+    public void setclassName(String cName){
+        className= new SimpleStringProperty(cName);
+    }
+
+    public String getCurrentTime(){
+        return currentTime.get();
+    }
+
+    public void setCurrentTime(String time){
+        currentTime= new SimpleStringProperty(time);
     }
 }
