1570c0a02470bab3f5bf399b6b4065c8a532418d
alexandre <alexadre>
2016-12-01 09:40:49 +0100

    Change log type format.

--- src/main/java/spoon/processor/ObjectCreationProcessor.java
+++ src/main/java/spoon/processor/ObjectCreationProcessor.java
@@ -7,16 +7,14 @@ import spoon.reflect.code.CtConstructorCall;
 import spoon.reflect.code.CtStatement;
 import spoon.reflect.reference.CtTypeReference;
 
+import java.time.Instant;
+
 /**
  * Created by alexandre on 18/11/16.
  */
 public class ObjectCreationProcessor extends AbstractProcessor<CtConstructorCall> {
 
     public void process(CtConstructorCall element) {
-
-        // create log messages
-        CtCodeSnippetStatement toPrintBefore = this.getFactory().Code().createCodeSnippetStatement("System.out.println(\"Begin creation\")");
-        CtCodeSnippetStatement toPrintAfter = this.getFactory().Code().createCodeSnippetStatement("System.out.println(\"End creation\")");
 
         // if affectation ("O o = new O()")
         if(!(element.getParent(CtStatement.class) instanceof CtBlock)){
@@ -30,16 +28,27 @@ public class ObjectCreationProcessor extends AbstractProcessor<CtConstructorCall
 
     // Function to add prints into log (without affectation)
     private void addLogs(CtStatement element, CtTypeReference type){
-        CtCodeSnippetStatement toPrintBefore = this.getFactory().Code().createCodeSnippetStatement("Logger.log(\"Begin " + type + " creation\")");
-        CtCodeSnippetStatement toPrintAfter = this.getFactory().Code().createCodeSnippetStatement("Logger.log(\"End " + type +" creation\")");
+        CtCodeSnippetStatement toPrintBefore = this.getFactory().Code().createCodeSnippetStatement(
+                "Logger.log(\"Begin object creation\" , \""+type+"\" , \"anonymous\");"
+        );
+
+        CtCodeSnippetStatement toPrintAfter = this.getFactory().Code().createCodeSnippetStatement(
+                "Logger.log(\"End object creation\" , \""+type+"\" , \"anonymous\");"
+        );
 
         element.insertBefore(toPrintBefore);
         element.insertAfter(toPrintAfter);
     }
     // Function to add prints into log, it prints the name of the object created.
     private void addLogs(CtStatement element, CtTypeReference type, String objectCreatedName){
-        CtCodeSnippetStatement toPrintBefore = this.getFactory().Code().createCodeSnippetStatement("Logger.log(\"Begin " + type + " " + objectCreatedName + " creation\")");
-        CtCodeSnippetStatement toPrintAfter = this.getFactory().Code().createCodeSnippetStatement("Logger.log(\"End " + type + " " + objectCreatedName + " creation\")");
+
+        CtCodeSnippetStatement toPrintBefore = this.getFactory().Code().createCodeSnippetStatement(
+                "Logger.log(\"Begin object creation\" , \""+type+"\" , \""+objectCreatedName+"\");"
+        );
+
+        CtCodeSnippetStatement toPrintAfter = this.getFactory().Code().createCodeSnippetStatement(
+                "Logger.log(\"End object creation\" , \""+type+"\" , \""+objectCreatedName+"\");"
+        );
 
         element.insertBefore(toPrintBefore);
         element.insertAfter(toPrintAfter);
