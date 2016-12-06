e17c234ce1c9791475140dc4b0c47d0a28bfd70b
alexandre <alexadre>
2016-12-06 10:11:32 +0100

    Solve bug with void method.

--- src/main/java/spoon/processor/MethodsCallProcessor.java
+++ src/main/java/spoon/processor/MethodsCallProcessor.java
@@ -18,15 +18,26 @@ public class MethodsCallProcessor extends AbstractProcessor<CtMethod>{
     // Called for each method call
     public void process(CtMethod element) {
 
+        // Code to insert
         CtCodeSnippetStatement toPrintBefore = this.getFactory().Code().createCodeSnippetStatement(
                 "Logger.logBeginMethodCall(\""+element.getSimpleName()+"\");"
         );
-        element.getBody().insertBegin(toPrintBefore);
 
         CtCodeSnippetStatement toPrintAfter = this.getFactory().Code().createCodeSnippetStatement(
                 "Logger.logEndMethodCall(\""+element.getSimpleName()+"\");"
         );
-        element.getBody().insertBefore(new ReturnOrThrowFilter(),toPrintAfter);
+
+        // Insert at the beginning of the method body
+        element.getBody().insertBegin(toPrintBefore);
+
+        // Insert before a return or a a throw if not void method
+        if(element.getType().getSimpleName() != "void") {
+            element.getBody().insertBefore(new ReturnOrThrowFilter(),toPrintAfter);
+        }
+        // Insert at the end of the method body (in case of void method)
+        else {
+            element.getBody().insertEnd(toPrintAfter);
+        }
     }
 
 }
